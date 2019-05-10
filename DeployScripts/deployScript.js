b12408bd73b242b8eef5b32c43fc73beae49e88f
Scott Samson <ssamson@associaonline.com>
2019-05-09 21:37:53 -0600

    next version

--- DeployScripts/deployScript.js
+++ DeployScripts/deployScript.js
@@ -1,11 +1,12 @@
 var program = require('commander');
 var AWS = require('aws-sdk');
-var async = require('async');
+var fs = require("fs");
+const path = require("path");
 
 program
-    .option('-a, --access_key')
-    .option('-s, --secret_key')
-    .option('-r, --region <us-east-1>')
+    .option('-a, --access_key <AKIAIOSFODNN7EXAMPLE>', 'Access key ID for the AWS account to run calls under.')
+    .option('-s, --secret_key <wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY>', 'Secret access key for the AWS account to sign calls with.')
+    .option('-r, --region <us-west-2>', 'AWS region to create infrastructure in.')
     .parse(process.argv);
 
 if (!program.access_key || !program.secret_key || !program.region) {
@@ -18,7 +19,46 @@ AWS.config.update({
     "secretAccessKey": program.secret_key,
     "region": program.region
 });
+console.log("stack update begining");
 
-async.waterfall([
-  () => { }
-]);
+var cf = new AWS.CloudFormation();
+
+fetchFileFromDisk("../Templates/demoStack.json", function (error, data) {
+  if (error) {
+    console.log();
+    console.log("error getting stack from disk");
+    console.log();
+    console.log(error.message);
+    return;
+  }
+
+  var params = {
+    StackName: "DemoApp",
+    Capabilities: ['CAPABILITY_NAMED_IAM'],
+    TemplateBody: data
+  }
+
+  cf.updateStack(params, callback)
+});
+
+
+function callback(error) {
+  if (error) {
+    console.log(error);
+  }
+  else {
+    console.log("stack update complete");
+  }
+};
+
+function fetchFileFromDisk(filePath, returnFn) {
+    console.log();
+    console.log("fetchFileFromDisk called for " + filePath);
+    fs.readFile(path.resolve(__dirname, filePath), "utf8", function (error, data) {
+        if (error) {
+            return returnFn(error);
+        } else {
+            return returnFn(null, data);
+        }
+    })
+};
